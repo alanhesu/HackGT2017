e7122a25f2c37ede7d86923051eaeb549f6256e6
alanhesu <alanhesu@gmail.com>
2017-10-14 17:21:40 -0400

    Camera and test flap

--- Assets/Scripts/Dodo.cs
+++ Assets/Scripts/Dodo.cs
@@ -2,22 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dodo : MonoBehaviour {   
-    public Vector2 pos;
+public class Dodo : MonoBehaviour {    
+    private float timer = 0;
+    private float HandRightPrevZ;
+    private float HandLeftPrevZ;
+    private float timerPeriod = .05f;
+    private Rigidbody rb;
+    public float thrust;
 
 	// Use this for initialization
 	void Start () {
-		
+        StartFlying(GameController.launchHeight);
+        rb = GetComponent<Rigidbody>();
+        rb.mass = 10;
+        thrust = 100;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () {        
+        /*
+        timer += Time.deltaTime;       
+        if (timer >= timerPeriod && KinectManager.instance.IsAvailable)
+        {
+            if ((HandRightPrevZ - KinectManager.instance.HandRight.Z > .5f)
+                && (HandLeftPrevZ - KinectManager.instance.HandLeft.Z > .5f))
+            {
+                rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+                HandRightPrevZ = KinectManager.instance.HandRight.Z;
+                HandLeftPrevZ = KinectManager.instance.HandLeft.Z;
+                timer = 0;
+            }
+        } */
+        if (Input.GetKeyDown("up"))
+        {
+            rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+        }       
 	}
 
     void StartFlying(float startHeight)
     {
-        pos.x = 0;
-        pos.y = startHeight;
+        transform.position = new Vector3(0, startHeight, 0);        
     }
 }
