77f978e7dc5deab40512ad099899fdff9eae09cc
lealex262 <lealex262@gmail.com>
2017-10-14 17:22:13 -0400

    Kinect Manager

--- Assets/Scripts/KinectManager.cs
+++ Assets/Scripts/KinectManager.cs
@@ -15,8 +15,28 @@ public class KinectManager : MonoBehaviour {
 
     public GameObject kinectAvailableText;
     public Text handXText;
-
+    private ulong personID = 0;
     public bool IsAvailable;
+
+    public Vector3 handLeft;
+    public Vector3 handRight;
+
+    public static KinectManager instance = null;
+
+    public Body[] GetBodies()
+    {
+        return _bodies;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+    }
 
     // Use this for initialization
     void Start () {
@@ -42,17 +62,82 @@ public class KinectManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         IsAvailable = _sensor.IsAvailable;
+        CameraSpacePoint postion;
 
         if (_bodyFrameReader != null)
         {
             var frame = _bodyFrameReader.AcquireLatestFrame();
+
             if (frame != null)
             {
                 frame.GetAndRefreshBodyData(_bodies);
 
+                foreach (var body in _bodies.Where(b => b.IsTracked))
+                {
+                    IsAvailable = true;
 
+                    if (body.TrackingId == personID)
+                    {
+                        postion = body.Joints[JointType.HandLeft].Position;
+                        handLeft = new Vector3(postion.X, postion.Y, postion.Z);
+                        postion = body.Joints[JointType.HandRight].Position;
+                        handRight = new Vector3(postion.X, postion.Y, postion.Z);
+
+                    } else {
+                        personID = Calibrate();
+                    }
+
+                }
+            }
+
+            frame.Dispose();
+            frame = null;
+        }
+
+    }
+
+    //Calibrate which person to track
+    ulong Calibrate ()
+    {
+        float[] distance = new float[_bodies.Length];
+        int i = 0;
+        CameraSpacePoint position;
+        foreach (Body pers in _bodies) {
+            position = pers.Joints[JointType.Head].Position;
+            distance[i] = Mathf.Sqrt((position.X * position.X) + (position.Y * position.Y) + (position.Z * position.Z));
+            i++;
+        }
+
+        float min = distance[i];
+        ulong id = _bodies[i].TrackingId;
+        for (i = 0; i < distance.Length; i++) {
+            if (min > distance[i])
+            {
+                min = distance[i];
+                id = _bodies[i].TrackingId;
             }
         }
 
+        return id;
+    }
+
+    void OnApplicationQuit()
+    {
+        if (_bodyFrameReader != null)
+        {
+            _bodyFrameReader.IsPaused = true;
+            _bodyFrameReader.Dispose();
+            _bodyFrameReader = null;
+        }
+
+        if (_sensor != null)
+        {
+            if (_sensor.IsOpen)
+            {
+                _sensor.Close();
+            }
+
+            _sensor = null;
+        }
     }
 }
