6541129a6e1bb01a31446e49ce9c2674d00d1cdb
lealex262 <lealex262@gmail.com>
2017-10-14 14:46:27 -0400

    Setup kinect

--- Assets/Scripts/KinectManager.cs
+++ Assets/Scripts/KinectManager.cs
@@ -1,16 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using Windows.Kinect;
+
+using System.Linq;
 
 public class KinectManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private KinectSensor _sensor;
+    private BodyFrameReader _bodyFrameReader;
+    private Body[] _bodies = null;
+
+    public GameObject kinectAvailableText;
+    public Text handXText;
+
+    public bool IsAvailable;
+
+    // Use this for initialization
+    void Start () {
+        _sensor = KinectSensor.GetDefault();
+
+        if (_sensor != null)
+        {
+            IsAvailable = _sensor.IsAvailable;
+
+            kinectAvailableText.SetActive(IsAvailable);
+
+            _bodyFrameReader = _sensor.BodyFrameSource.OpenReader();
+
+            if (!_sensor.IsOpen)
+            {
+                _sensor.Open();
+            }
+
+            _bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        IsAvailable = _sensor.IsAvailable;
+
+        if (_bodyFrameReader != null)
+        {
+            var frame = _bodyFrameReader.AcquireLatestFrame();
+            if (frame != null)
+            {
+                frame.GetAndRefreshBodyData(_bodies);
+
+
+            }
+        }
+
+    }
 }
